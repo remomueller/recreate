92ee8062f969ff1987c410be68926871dd1025d6
Remo Mueller <remosm@gmail.com>
2019-02-08 23:34:43 -0500

    Enable gravity on click, and reset velocity to zero on second click

--- Assets/WallDestruction.cs
+++ Assets/WallDestruction.cs
@@ -2,9 +2,15 @@
 
 public class WallDestruction : MonoBehaviour
 {
+    public Rigidbody rb;
+
     private void OnMouseDown()
     {
-        gameObject.SetActive(false);
+        // gameObject.SetActive(false);
         Debug.Log("On Mouse Down Wall destruction,");
+        rb.useGravity = !rb.useGravity;
+        if (!rb.useGravity) {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
